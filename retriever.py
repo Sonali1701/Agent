44b509a78534ae078757d51acc68b711389da20a
Sonali1701 <vs5106717@gmail.com>
2025-05-08 15:33:46 +0530

    change7

--- retriever.py
+++ retriever.py
@@ -1,7 +1,8 @@
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 
-embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+embedding_model = HuggingFaceEmbeddings(model_name="intfloat/e5-small-v2")
+
 
 def get_top_chunks(query, k=3):
     # Enable deserialization by setting allow_dangerous_deserialization=True
